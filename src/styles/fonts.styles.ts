4b688ccc8d5a5ea9de8b36c45c439400003899eb
Chloe Montgomery <chloemontgomery01@gmail.com>
2020-05-04 14:16:05 +0100

    Change text to favorit light

--- src/styles/fonts.styles.ts
+++ src/styles/fonts.styles.ts
@@ -1,12 +1,12 @@
 import { createGlobalStyle } from 'styled-components';
 
 // @ts-ignore
-import FavoritRegular from './favorit-regular.woff';
+import FavoritLight from './favorit-light.woff';
 
 export default createGlobalStyle`
     @font-face {
         font-family: 'Favorit';
         src: local('Favorit'), local('Favorit'),
-        url(${FavoritRegular}) format('woff');
+        url(${FavoritLight}) format('woff');
     }
 `;
